c0846f305c119f175941e2b787a3ed4cc29182ef
Alaktus <alancorreas@gmail.com>
2013-10-31 19:01:23 -0200

    Criacao da classe de Conexao com o banco de dados

--- Programacao/BibliotecaTRM5/src/br/com/spei/bibliotecatrm5/mvc/dao/Conexao.java
+++ Programacao/BibliotecaTRM5/src/br/com/spei/bibliotecatrm5/mvc/dao/Conexao.java
@@ -3,10 +3,13 @@ package br.com.spei.bibliotecatrm5.mvc.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+
+import br.com.spei.bibliotecatrm5.util.ConfigurationFile;
 
 public class Conexao {
 	// TODO Configurar externamente
-	private String connection = "jdbc:sqlserver://ZEUS;database=BANCO_CLIENTE;user=AulaJava;password=123";
+	private String connection = "jdbc:sqlserver://{SERVER};database={DATABASE};user={USER};password={PWD}";
 	private static Conexao instance; 
 	
 	private Conexao() {
@@ -15,12 +18,31 @@ public class Conexao {
 	
 	public static Conexao getInstance() {
 		if(instance == null)
+		{
 			instance = new Conexao();
+			instance.configuraStringConexao();
+		}
 		
 		return instance;
 	}
 	
-	public Connection getConnection() throws SQLException {
+	private void configuraStringConexao() {
+		ConfigurationFile configurador = new ConfigurationFile();
+		Properties propriedades = configurador.obtemPropriedadesArquivoConfiguracao();
+		String servidor = propriedades.getProperty("SERVER");
+		String banco = propriedades.getProperty("DB");
+		String usuario = propriedades.getProperty("USER");
+		// TODO Descriptografar senha
+		String senha = propriedades.getProperty("PWD");
+		
+		instance.connection = instance.connection.
+								replace("{SERVER}", servidor).
+								replace("{DATABASE}", banco).
+								replace("{USER}", usuario).
+								replace("{PWD}", senha);
+	}
+
+	public Connection getConnection() throws SQLException {		
 		return DriverManager.getConnection(connection);
 	}
 }
