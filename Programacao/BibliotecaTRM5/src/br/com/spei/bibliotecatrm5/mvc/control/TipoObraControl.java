32f0eccab3419aa1718469df728665bb0730a815
Alaktus <alancorreas@gmail.com>
2013-10-31 08:09:40 -0200

    Ajustes no controlador e frame TipoObra

--- Programacao/BibliotecaTRM5/src/br/com/spei/bibliotecatrm5/mvc/control/TipoObraControl.java
+++ Programacao/BibliotecaTRM5/src/br/com/spei/bibliotecatrm5/mvc/control/TipoObraControl.java
@@ -2,6 +2,11 @@ package br.com.spei.bibliotecatrm5.mvc.control;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 import br.com.spei.bibliotecatrm5.mvc.model.TipoObra;
 import br.com.spei.bibliotecatrm5.mvc.view.FrameTipoObra;
@@ -13,14 +18,18 @@ public class TipoObraControl implements ActionListener {
 	
 	public TipoObraControl(FrameTipoObra view) {
 		this.view = view;
+		view.configuraOuvinteAcao(this);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+		if(e.getActionCommand().equalsIgnoreCase("cancelar")) {
+			view.setVisible(false);
+		}
 	}
 
 	public void inicia() {
 		view.setVisible(true);
+//		view.configuraOuvinteFoco(this);
 	}
 }
